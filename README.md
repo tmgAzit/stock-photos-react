778f7cf7628f943cb0f3fc53f1ade9494267e2a0
tmgAzit <azit.ghising@gmail.com>
2022-05-27 15:31:42 +0930

    create .env file with access key.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-access key: 4eVQ9hkFPABjJNiJbjY7A7ZvNvlPxPda1Y4EmnzdLKY
-secret key: \_bOd1lD9GsdRK_y6fthztffypsebT_hOeQmg8Jgb6Zw
+
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
@@ -13,7 +14,7 @@ function App() {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=4eVQ9hkFPABjJNiJbjY7A7ZvNvlPxPda1Y4EmnzdLKY;`;
+    url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
