1eb5033c171605695a550beba8bf497dacc8e4ad
tmgAzit <azit.ghising@gmail.com>
2022-05-27 15:16:39 +0930

    initial fetch is define.

---
+++ README.md
@@ -0,0 +1,2 @@
+access key: 4eVQ9hkFPABjJNiJbjY7A7ZvNvlPxPda1Y4EmnzdLKY
+secret key: \_bOd1lD9GsdRK_y6fthztffypsebT_hOeQmg8Jgb6Zw
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}?client_id=4eVQ9hkFPABjJNiJbjY7A7ZvNvlPxPda1Y4EmnzdLKY;`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  return <h2>stock photos starter</h2>;
+}
+
+export default App;
